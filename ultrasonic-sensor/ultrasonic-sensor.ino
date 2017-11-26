f1b0fb97fb28fd71b70131eb571615077f8f9cc3
Gede Lumbung <gedesumawijaya@gmail.com>
2017-11-26 17:46:22 +0800

    :sparkles: ultrasonic sensor with led buzzer indicator

---
+++ ultrasonic-sensor/ultrasonic-sensor.ino
@@ -0,0 +1,55 @@
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+const int buzzer = 11;
+const int ledPin = 13;
+
+// defines variables
+long duration;
+int distance;
+int safetyDistance;
+
+
+void setup() {
+pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+pinMode(buzzer, OUTPUT);
+pinMode(ledPin, OUTPUT);
+Serial.begin(9600); // Starts the serial communication
+}
+
+
+void loop() {
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+
+safetyDistance = distance;
+if (safetyDistance <= 20){
+  digitalWrite(buzzer, HIGH);
+  delay(20);
+  digitalWrite(buzzer, LOW);
+  delay(20);
+  digitalWrite(ledPin, HIGH);
+  delay(20);
+  digitalWrite(ledPin, LOW);
+  delay(20);
+}
+else{
+  digitalWrite(buzzer, LOW);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  delay(100);
+}
+}
